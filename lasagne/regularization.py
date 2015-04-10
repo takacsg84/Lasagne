7e8842e059e7eb3868f6db18b173d0b07ff5a29e
takacsg84 <takacsg84@gmail.com>
2015-04-10 23:28:09 +0200

    Adding some batch norm related stuff

--- lasagne/regularization.py
+++ lasagne/regularization.py
@@ -9,7 +9,15 @@ def l2(layer, include_biases=False):
     else:
         all_params = layers.get_all_non_bias_params(layer)
 
-    return sum(T.sum(p**2) for p in all_params)
+    return sum(T.sum(p ** 2) for p in all_params)
 
+
+def l1(layer, include_biases=False):
+    if include_biases:
+        all_params = layers.get_all_params(layer)
+    else:
+        all_params = layers.get_all_non_bias_params(layer)
+
+    return sum(T.sum(abs(p)) for p in all_params)
 
 # TODO: sparsity regularization
